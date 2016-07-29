623c764844288b22c4a1da08bbd4a7dded5862c1
liujianpc <695966004@qq.com>
2016-07-30 00:43:26 +0800

    add offlineBroadCastReceiver

---
+++ OffLineForeceTest/src/com/example/offlineforecetest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.offlineforecetest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		final LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(MainActivity.this);
+		Button sendButton = (Button) findViewById(R.id.sendBroadcast);
+		sendButton.setOnClickListener(new OnClickListener() {
+		
+		
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent("com.example.offlinebroadcastreceiver.MY_OFFLINE");
+				localBroadcastManager.sendBroadcast(intent);
+
+			}
+		});
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("com.example.offlinebroadcastreceiver.MY_OFFLINE");
+		OfflineBroadCastReceiver offlineBroadCastReceiver = new OfflineBroadCastReceiver();
+		localBroadcastManager.registerReceiver(offlineBroadCastReceiver, intentFilter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	class OfflineBroadCastReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			ActivityCollector.finishAllActivity();
+			Toast.makeText(context, "强制下线", Toast.LENGTH_LONG).show();
+			Intent intent2 = new Intent(MainActivity.this, LoginActivity.class);
+			startActivity(intent2);
+			
+		}
+		
+	}
+
+
+}
+
